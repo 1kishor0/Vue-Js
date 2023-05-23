cf684a6f70d551ed003defe3c17ceacc94dda732
kishorsakib10@gmail.com <kishorsakib10@gmail.com>
2023-05-23 18:14:11 +0600

    second

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ new Vue({
 
     el: '#app',
     data:{
+        src: "https://picsum.photos/seed/picsum/200/300",
+        alt: "This is a great viewing experience",
         title: "Sample",
         isItTrue: true,
         cars:['Ford', 'Toyota'],
@@ -11,6 +13,55 @@ new Vue({
         },
         greeting:()=>{
             return "Hello World";
-        }
-    }
+        },
+
+        myText: "This is a great viewing experience",
+        myHtml: "<strong> A manual car </strong>",
+
+        x: 20,
+        y: 30,
+        z:40,
+        p:0,
+        q:0,
+        
+
+        user2:{
+            names:"John",
+            Ages:"24",
+            Dur:"3 months ago",
+        },
+        name:"John",
+        named:"boy"
+
+    },
+    methods: {
+         fullName :()=>{
+            return "Sheikh Rahmatulla Sakib";
+        },
+
+        changedUsers:()=>{
+            return (this.z > this.y)? true:false ;
+    },
+    update(){
+        setTimeout(()=>{
+                this.name="Rafee";
+        },2000);
+    
+    },
+
+    updateUser(){
+        this.named="Rafee";
+    },
+
+    getcoord(event){
+
+        this.p=event.clientX;
+        this.q=event.clientY;
+    },
+    
+    
+    
+
+
+}
 });
