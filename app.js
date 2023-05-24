11e782e6dc5962caf6364ab1a9d2914ddd4371a5
kishorsakib10@gmail.com <kishorsakib10@gmail.com>
2023-05-24 16:24:28 +0600

    3rd day

--- app.js
+++ app.js
@@ -23,6 +23,11 @@ new Vue({
         z:40,
         p:0,
         q:0,
+        salary:10,
+        s:0,
+        r:0,
+        n:0,
+        z:0,
         
 
         user2:{
@@ -31,9 +36,19 @@ new Vue({
             Dur:"3 months ago",
         },
         name:"John",
-        named:"boy"
+        named:"boy",
+        rename:"John",
 
+        formData:{
+            firstName:"",
+            lastName:"",
+        },
     },
+
+
+
+
+
     methods: {
          fullName :()=>{
             return "Sheikh Rahmatulla Sakib";
@@ -53,15 +68,66 @@ new Vue({
         this.named="Rafee";
     },
 
-    getcoord(event){
+    getcoord(event) {
+        this.p = event.clientX;
+        this.q = event.clientY;
+        
+        if (this.p === 0 && this.q === 0) {
+          // Update values outside the hover div
+          // For example:
+          this.p = 10;
+          this.q = 20;
+        }
+      },
 
-        this.p=event.clientX;
-        this.q=event.clientY;
-    },
-    
-    
-    
+      namechanger(newname,event){
+        this.rename=newname;
+        console.log(event);
+
+      },
+      handleform(){
+        console.log(this.formData);
+
+      },
+  
 
 
+},
+computed:{
+    addtoA(){
+        console.log("ADD A");
+        return this.n+this.salary;
+      },
+      addtoB(){
+        console.log("ADD B");
+        return this.z+this.salary;
+      }
 }
+});
+
+const app2=new Vue({
+    el:"#app1",
+
+    data:{
+        value:100,
+    },
+
+    methods: {
+        change(){
+            app3.value="changed";
+          return this.value;
+        }
+    },
+
+
+});
+
+const app3=new Vue({
+    el:"#app2",
+
+    data:{
+        value:100,
+    },
+
+
 });
